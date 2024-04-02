f8c61f964f8c42ab33fe7afd0525f09d7e2c9d37
John Kingh <Kingh-J@ulster.ac.uk>
2024-04-02 10:06:11 +0100

    login test

---
+++ backend/routes/__tests__/loginRoute.test.js
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import Login from '../../Schema/MyLogin';
+import app from '../../app';
+
+// Declare a variable to hold the MongoDB Memory Server
+let mongoServer;
+
+// Before all tests, start the MongoDB Memory Server and establish a connection
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// After all tests, stop the MongoDB Memory Server and close the connection
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+
+describe('POST /login', () => {
+  test('should return 401 if  password is missing', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({ username: 'jkingh' });
+
+    expect(res.status).toBe(401);
+    expect(res.body).toMatchObject({
+     error: 'Invalid username or password'
+    });
+  });
+
+
+});
+describe('POST /login', () => {
+  test('should return 401 if  or password is missing', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({ });
+
+    expect(res.status).toBe(401);
+    expect(res.body).toMatchObject({
+      error: 'Invalid username or password',
+    });
+  });
+
+  
+});
